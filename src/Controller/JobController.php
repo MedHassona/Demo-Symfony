8210bb123cb6bd7ec7df17ba59e56da9e500a2ad
mohamed hassona <42044451+MedHassona@users.noreply.github.com>
2019-03-04 17:20:08 +0000

    Update JobController.php

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -12,19 +12,16 @@ class JobController extends AbstractController
     public function index(EntityManagerInterface $em)
     {
         $jobs = $em->getRepository(Job::class)->findAll();
-
         return $this->render('job/index.html.twig', [
             'jobs' => $jobs,
         ]);
     }
 
-    public function show(EntityManagerInterface $em, int $id)
-    {
+    public function show(EntityManagerInterface $em, int $id){
         $job = $em->getRepository(Job::class)->find($id);
         if(null === $job){
             throw new NotFoundHttpException();
         }
-
         return $this->render('job/show.html.twig', [
             'job' => $job,
         ]);
